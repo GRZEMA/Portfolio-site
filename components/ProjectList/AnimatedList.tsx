9495ec5815ecdad455e4f9119b71699db2fe86d3
GRZEMA <dawidkrzmnsk@gmail.com>
2023-09-14 19:05:26 +0200

    Cosmetic changes

--- components/ProjectList/AnimatedList.tsx
+++ components/ProjectList/AnimatedList.tsx
@@ -12,7 +12,9 @@ const AnimatedList = ({ items }: AnimatedListProps): JSX.Element => {
 					initial={{ opacity: 0, x: -100 }}
 					whileInView={{ opacity: 1, x: 0 }}
 					viewport={{ once: true }}
-					className='relative before:absolute before:w-2 before:h-2 md:before:h-3 md:before:w-3 before:rotate-45 before:bg-text before:left-0 pl-4 md:pl-8 before:top-1/2 before:-translate-y-1/2 hover:text-customYellow transition-colors duration-300'
+					className={`relative before:absolute before:w-2 before:h-2 md:before:h-3 md:before:w-3 before:rotate-45 before:bg-text before:left-0 pl-4 md:pl-8 before:top-1/2 before:-translate-y-1/2 transition-colors duration-300 ${
+						(item.link || item.email) && 'hover:text-customYellow'
+					}`}
 					key={item.span ? item.span : item.text}>
 					{item.span ? (
 						<span className='font-bold text-customYellow'>{item.span} </span>
