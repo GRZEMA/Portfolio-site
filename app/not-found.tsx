d50dd54b9d498a3beca1f5e2f2d4b30ae6559b0f
GRZEMA <dawidkrzmnsk@gmail.com>
2023-09-14 18:34:22 +0200

    404 page added

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Button from '@/components/UI/Button'
+
+const Error = (): JSX.Element => {
+	return (
+		<main className='w-full h-screen items-center justify-center'>
+			<section className='w-full h-full flex flex-col items-center justify-evenly bg-background relative'>
+				<video src='/assets/404.mp4' autoPlay={true} loop={true}></video>
+				<Button size='2xl' href='/'>
+					Go back to Home Page
+				</Button>
+			</section>
+		</main>
+	)
+}
+
+export default Error
